4ed130d32ebac5ef24235817a79a85d8cfe4fcfc
ryhaldenys <denysryhal24@gmail.com>
2023-04-30 20:28:45 +0300

    create config service

---
+++ ConfigService/src/main/java/ua/nulp/kn303/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.nulp.kn303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+
+}
---
+++ ConfigService/src/test/java/ua/nulp/kn303/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nulp.kn303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
