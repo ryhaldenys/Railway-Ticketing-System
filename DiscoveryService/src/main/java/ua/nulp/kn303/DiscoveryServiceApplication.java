68b4a49e2935ccef9319d79bd4c57761b79dbb99
ryhaldenys <denysryhal24@gmail.com>
2023-05-01 00:07:39 +0300

    added new setting to discovery service

--- DiscoveryService/src/main/java/ua/nulp/kn303/DiscoveryServiceApplication.java
+++ DiscoveryService/src/main/java/ua/nulp/kn303/DiscoveryServiceApplication.java
@@ -2,7 +2,9 @@ package ua.nulp.kn303;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
